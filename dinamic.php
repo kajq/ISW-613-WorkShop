0fe8727b12f446511b32f806005a3271f63ee09b
Keilor Jimenez Quesada <keilorjimenez95@gmail.com>
2018-09-18 20:18:52 -0600

    Up index.html and dinamic.php

---
+++ dinamic.php
@@ -0,0 +1,28 @@
+<?php
+$var= 'Hello Dinamic Word';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dinamic</title>
+</head>
+<body>
+<div>
+	<li>
+		<a href="index.html"> Static </a>
+	</li>
+	<li>
+		<a href="dinamic.php"> Dinamic </a>
+	</li>
+</div>
+<br>
+<?php
+echo '<h3>';
+echo $var;
+echo '</h3>';
+echo '<img src="https://cdn-images-1.medium.com/max/1600/1*jB76MLZjiNhGSQQvxm7LSQ.gif">';
+?>
+
+</body>
+</html>
